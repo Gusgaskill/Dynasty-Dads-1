9e1f7dbb48e81d662ecb9aa21f4af1edb6419444
Gusgaskill <138895338+Gusgaskill@users.noreply.github.com>
2026-02-15 11:11:53 -0600

    Update league ID in leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1313137559294988288"; // your league ID
+export const leagueID = "1312130532338827264"; // your league ID
 export const leagueName = "Dynasty Dads 1"; // your league name
 export const dues = 20; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
